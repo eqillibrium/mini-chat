adfcb5284f61a07271541ad1dd0f2402fa1a3c98
Naerdel <eqillibrium@live.ru>
2022-07-04 16:53:56 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WebSock from "./WebSock";
+
+function App() {
+  return (
+    <div className="App">
+     <WebSock />
+    </div>
+  );
+}
+
+export default App;
